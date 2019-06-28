3e0b00411a7dfa5c2ba7a8d7bec69e1f40047a0d
mgojak1 <mgojak1@etf.unsa.ba>
2019-06-28 19:44:31 +0200

    ubacene klase fiksni i medunarodni broj

--- src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/FiksniBroj.java
@@ -1,4 +1,29 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class FiksniBroj {
+public class FiksniBroj extends TelefonskiBroj {
+    public FiksniBroj(Grad grad, String broj) {
+        this.grad = grad;
+        this.broj = broj;
+    }
+
+
+    public enum Grad {
+        TRAVNIK, ORASJE, ZENICA, SARAJEVO, LIVNO, TUZLA,
+        MOSTAR, BIHAC, GORAZDE, SIROKIBRIJEG, BRCKO, MRKONJICGRAD,
+        BANJALUKA, PRIJEDOR, DOBOJ, SAMAC, BIJELJINA, ZVORNIK, PALE,
+        FOCA, TREBINJE
+    }
+    private Grad grad;
+    private String broj;
+    private String pozivni[] = {"030", "031", "032", "033", "034", "035", "036", "037",
+            "038", "039", "049", "050", "051", "052", "053", "054", "055", "056", "057", "058", "059"};
+
+    @Override
+    public String Ispisi() {
+        return pozivni[grad.ordinal()] + "/" + broj;
+    }
+    @Override
+    public int hashCode() {
+        return (Integer.parseInt(broj) + 5) * 7;
+    }
 }
--- src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class MedunarodniBroj {
+public class MedunarodniBroj extends  TelefonskiBroj {
+    private String drzava;
+    private String broj;
+
+    public MedunarodniBroj(String drzava, String broj) {
+        this.drzava = drzava;
+        this.broj = broj;
+    }
+
+
+    @Override
+    public String Ispisi() {
+        return drzava + "/" + broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
