d25aa5e5fe65301115eb8ff7ab5ec684d4e33289
mgojak1 <mgojak1@etf.unsa.ba>
2019-06-29 11:19:19 +0200

    ubacen mobilni broj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
-public class MobilniBroj {
+public class MobilniBroj extends TelefonskiBroj {
+    private Integer mobilnaMreza;
+    private String broj;
+
+    public MobilniBroj(int mobilnaMreza, String broj) {
+        this.mobilnaMreza = mobilnaMreza;
+        this.broj = broj;
+    }
+
+    @Override
+    public String Ispisi() {
+        return "0" + Integer.toString(mobilnaMreza) + "/" + broj;
+    }
+
+    @Override
+    public int hashCode() {
+        return (Integer.parseInt(broj) + 5) * 7;
+    }
 }
