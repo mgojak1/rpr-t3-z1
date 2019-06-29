f4d6d5c33322b3af7f7c37aad2347d733554e55a
mgojak1 <mgojak1@etf.unsa.ba>
2019-06-29 11:54:27 +0200

    imenik

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,4 +1,23 @@
 package ba.unsa.etf.rpr.tutorijal03;
 
+import java.util.*;
+
 public class Imenik {
+    private String ime;
+    private TelefonskiBroj broj;
+    private HashMap<TelefonskiBroj, String> hmap = new HashMap<>();
+
+    public void dodaj(String ime, TelefonskiBroj broj) {
+        hmap.putIfAbsent(broj, ime);
+    }
+
+    String dajBroj(String ime) {
+        return  broj.Ispisi();
+    }
+
+    String dajIme(TelefonskiBroj broj) {
+        return hmap.get(broj);
+    }
+
+
 }
